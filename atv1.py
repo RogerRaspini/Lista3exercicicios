12986c17cc496dd9bed6aa9edf9b1b068b55a606
RogerRaspini <128654705+RogerRaspini@users.noreply.github.com>
2023-03-29 22:41:35 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,9 @@
+raio = float(input('informe o raio do circulo:' ))
+pi = float(3.14159)
+área = pi * (raio **2)
+print('O raio do circulo é' , raio)
+alturaC = float(input('informe a altura do cilindro: '))
+áreaC = área * alturaC
+print ('A altura do cilindro é' , áreaC )
+
+
---
+++ atv2.py
@@ -0,0 +1,4 @@
+x = float(input('informe um valor:' ))
+resultado = (x **2) + (2 * x) - 4
+print ('o resultado da expressão é' , resultado)
+
